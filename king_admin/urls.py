42399a053428d4da517221c9a77bcbb7d19cd0e6
LeonQiuM <qiumeng627@gmail.com>
2018-01-09 10:10:34 +0800

    king admin html

--- king_admin/urls.py
+++ king_admin/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url,include
 from king_admin import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='stu_index'),
+    url(r'^$', views.index, name='table_index'),
 ]
--- king_admin/views.py
+++ king_admin/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    '''
+
+    :param request:
+    :return:
+    '''
+    if request.method == "GET":
+        return render(request,'king_admin/table_index.html')
+
+    elif request.method == "POST":
+        pass
+
+    else:
+        pass
