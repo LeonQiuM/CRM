d603c91cafaebafd056c2c3cef79e81636b4fe75
LeonQiuM <qiumeng627@gmail.com>
2018-01-10 09:18:16 +0800

    admin实现

--- king_admin/views.py
+++ king_admin/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from king_admin import king_admin
+import importlib
+
 
 # Create your views here.
 
@@ -10,7 +13,27 @@ def index(request):
     :return:
     '''
     if request.method == "GET":
-        return render(request,'king_admin/table_index.html')
+        table_list = king_admin.enabled_admins
+        print(table_list)
+        return render(request, 'king_admin/table_index.html', locals())
+
+    elif request.method == "POST":
+        pass
+
+    else:
+        pass
+
+
+def display_table_objs(request, app, table):
+    '''
+
+    :param request:
+    :return:
+    '''
+    print(app,table)
+    admin_class = king_admin.enabled_admins[app][table]
+    if request.method == "GET":
+        return render(request, 'king_admin/table_objs.html', locals())
 
     elif request.method == "POST":
         pass
